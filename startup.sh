0b03238c59eb02d24732b6f39b41ab70c7d113e9
Chaoyi Zhang <79236307+chaoyivision@users.noreply.github.com>
2022-05-11 17:42:43 -0700

    Update startup.sh
    check if exists

--- startup.sh
+++ startup.sh
@@ -1,11 +1,20 @@
-tmux new -d -s main; # the main session with all gpus available.
+sess="main"
+tmux has-session -t $sess 2>/dev/null
+if [ $? != 0 ]; then
+    tmux new -d -s "$sess"; # the main session with all gpus available.
+fi
+
 
 num_gpus=`nvidia-smi -L | wc -l`
-
 #echo $num_gpus
- 
+
 for i in $(seq 0 $((num_gpus-1)))
 do
-   tmux new -d -s "G$i";
-   tmux send-keys -t "G$i.0" "export CUDA_VISIBLE_DEVICES=$i" ENTER;
+   sess="G$i"
+   
+   tmux has-session -t $sess 2>/dev/null
+   if [ $? != 0 ]; then
+        tmux new -d -s "$sess";
+        tmux send-keys -t "$sess.0" "export CUDA_VISIBLE_DEVICES=$i" ENTER;
+   fi
 done
